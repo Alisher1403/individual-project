29865c1bea0c65363463de2cae7479812d496f2c
Alisher <alisherchinberdiyev1403@gmail.com>
2024-03-24 18:32:25 +0500

    bug fixes

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -4,7 +4,7 @@ import { RootState } from "store";
 import { VacancyCreate } from "pages";
 
 const Create: FC = () => {
-  const userType = useSelector((state: RootState) => state?.user?.type);
+  const userType = useSelector((state: RootState) => state.user.data?.type);
 
   if (userType === "applicant") {
     return <div></div>;
